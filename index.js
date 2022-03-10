da2d5da5a05df25ca9ff8e98c0e35f3d2a8c2de4
jotiba <87903172+jotibaharane@users.noreply.github.com>
2022-03-11 00:49:41 +0530

    Update index.js
    final

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ let age = document.getElementById("age");
 let player = document.getElementById("player");
 let add = document.getElementById("add");
 let list = document.getElementById("list");
+let inner=""
+let checkh=""
+let check=""
+let data =""
 var namesArr = [];
 function myFunction() {
+  funcheck();
+  if( namesArr.length <11){
   if (check.length === 0) {
-    if (data.Pname !== "" && data.Page !== "" && namesArr.length <= 11) {
+    if (data.Pname !== "" && data.Page !== "" ) {
       namesArr.push(data);
-      //   let new1 = [...namesArr, data];
       localStorage.setItem("PlayerData", JSON.stringify(namesArr));
       console.log("added");
     } else {
@@ -16,31 +21,41 @@ function myFunction() {
     }
   } else {
     alert("data alredy exist");
+  }}else{
+    alert("the player are full")
   }
+  render()
 man();
-  
+list.innerHTML = inner;
 }
 function man(){
-  let inner = `<tr>
-  <th>sr.no</th>
-  <th>Player Name</th>
-  <th>Player Age</th>
-  <th>Player Type</th>
-</tr>`;
-  let data = {
+  funcheck()
+  render();
+  list.innerHTML = inner;
+}
+function funcheck(){
+   data = {
     Pname: plname.value,
     Page: age.value,
     Ptype: player.value,
   };
-  let checkh = JSON.parse(localStorage.getItem("PlayerData"));  
+  checkh = JSON.parse(localStorage.getItem("PlayerData"));  
   // console.log(checkh.length)
   if (checkh) {
     namesArr=checkh
     console.log("localstorage check");
   }
-  let check = namesArr.filter((index) => {
+  check = namesArr.filter((index) => {
     return index.Pname === data.Pname;
   });
+}
+function render(){
+  inner = `<tr>
+  <th>sr.no</th>
+  <th>Player Name</th>
+  <th>Player Age</th>
+  <th>Player Type</th>
+  </tr>`;
   namesArr.map((index, no) => {
     return (inner += `<tr>
     <td>${no + 1}</td>
@@ -49,8 +64,6 @@ function man(){
     <td>${index.Ptype}</td>
     </tr>`);
   });
-  
-  list.innerHTML = inner;
 }
 man();
 add.addEventListener("click", myFunction);
